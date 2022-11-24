bcda0f878072edc6aa376e7ddeb76dfe69e80246
Nikita <nikon15151@yandex.ru>
2022-11-24 15:26:11 +0300

    add: modal window

---
+++ src/components/elements/modalWindow/ModalWindow.tsx
@@ -0,0 +1,66 @@
+import React from "react"
+import { CrossIcon } from "../icons"
+import styles from "./ModalWindow.module.scss"
+import utilStyles from "styles/utils.module.scss"
+
+export interface IModalWindowProps {
+    isShowing?: boolean,
+    onClose?: () => void
+    closable?: boolean,
+    children?: React.ReactNode,
+    backgroundOverlap?: boolean,
+    title?: string
+}
+
+const ModalWindow = ({
+    isShowing,
+    onClose = () => { },
+    closable,
+    children,
+    backgroundOverlap,
+    title
+}: IModalWindowProps) => {
+
+    const onBackgroundlClick = () => {
+        if (closable) onClose()
+    }
+
+    return (
+        <>
+            {isShowing &&
+                <>
+                    <div className={styles.container}>
+                        {closable &&
+                            <div
+                                className={styles.close_button}
+                                onClick={onClose}
+                            >
+                                <CrossIcon
+                                    width={15}
+                                    height={15}
+                                />
+                            </div>
+                        }
+                        <div className={styles.content}>
+                            {title &&
+                                <div className={utilStyles.modal_window_title}>
+                                    {title}
+                                </div>
+                            }
+                            {children}
+                        </div>
+                    </div>
+                    {backgroundOverlap &&
+                        <div
+                            className={styles.background}
+                            onClick={onBackgroundlClick}
+                        >
+                        </div>
+                    }
+                </>
+            }
+        </>
+    )
+}
+
+export default ModalWindow
