638b6cc7b25fe76ff3b89698c06495f82a842dc2
Nikita <nikon15151@yandex.ru>
2022-12-08 00:22:28 +0300

    upd: page content

--- src/components/templates/courses/index.tsx
+++ src/components/templates/courses/index.tsx
@@ -1,15 +1,69 @@
 import React from "react"
 import Layout from "components/layouts/Layout"
 import Head from "next/head"
+import ItemList from "components/modules/itemList"
+import styles from "./CoursesTemplate.module.scss"
+import useCourses from "./useCourses"
 
 const CoursesTemplate = () => {
+
+    const {
+        courses,
+        onCourseAddClick,
+        onCourseDeleteClick,
+        onCourseDetailsClick,
+        onCourseEditClick
+    } = useCourses()
 
     return (
         <Layout>
             <Head>
                 <title>Курсы</title>
             </Head>
-            Курсы
+            <div className={styles.courses_list_container}>
+                <ItemList
+                    className={styles.courses_list}
+                    headers={[
+                        {
+                            title: "Название",
+                            field: "title"
+                        },
+                        {
+                            title: "Описание",
+                            field: "description"
+                        },
+                        {
+                            title: "Дата создания",
+                            field: "createDate"
+                        },
+                        {
+                            title: "ID создателя",
+                            field: "creatorId"
+                        },
+                    ]}
+                    items={courses}
+                    itemControlButtons={[
+                        {
+                            title: "Детали",
+                            onClick: onCourseDetailsClick
+                        },
+                        {
+                            title: "Изменить",
+                            onClick: onCourseEditClick
+                        },
+                        {
+                            title: "Удалить",
+                            onClick: onCourseDeleteClick
+                        }
+                    ]}
+                    controlButtonsBottom={[
+                        {
+                            title: "Добавить",
+                            onClick: onCourseAddClick
+                        }
+                    ]}
+                />
+            </div>
         </Layout>
     )
 }
