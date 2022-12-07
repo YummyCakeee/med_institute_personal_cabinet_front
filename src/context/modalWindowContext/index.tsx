ac1dd0052172d84bdfb35fe6d20ff9e3c621beeb
Nikita <nikon15151@yandex.ru>
2022-12-08 00:21:55 +0300

    upd: course modal window

--- src/context/modalWindowContext/index.tsx
+++ src/context/modalWindowContext/index.tsx
@@ -1,41 +1,65 @@
 import ConfirmActionModalWindow from "components/modules/modalWindows/confirmActionModalWindow"
-import { IConfirmActionModalWindowProps } from "components/modules/modalWindows/confirmActionModalWindow"
+import { ConfirmActionModalWindowProps } from "components/modules/modalWindows/confirmActionModalWindow"
+import CourseModalWindow, { CourseModalWindowProps } from "components/modules/modalWindows/courseModalWindow"
 import React, { createContext, useContext, useState } from "react"
 
-interface IModalWindowContext {
-    setConfirmActionModalWindowState: React.Dispatch<React.SetStateAction<IConfirmActionModalWindowProps>>,
-    closeConfirmActionModalWindow: () => void
+interface ModalWindowContext {
+    setConfirmActionModalWindowState: React.Dispatch<React.SetStateAction<ConfirmActionModalWindowProps>>,
+    closeConfirmActionModalWindow: () => void,
+    setCourseModalWindowState: React.Dispatch<React.SetStateAction<CourseModalWindowProps>>,
+    closeCourseModalWindow: () => void
 }
 
-const ModalWindowContext = createContext<IModalWindowContext>({
+const ModalWindowContext = createContext<ModalWindowContext>({
     setConfirmActionModalWindowState: () => { },
-    closeConfirmActionModalWindow: () => { }
+    closeConfirmActionModalWindow: () => { },
+    setCourseModalWindowState: () => { },
+    closeCourseModalWindow: () => { }
 })
 
 
-const defaultConfirmActionModalWindowState: IConfirmActionModalWindowProps = {
+const defaultConfirmActionModalWindowState: ConfirmActionModalWindowProps = {
     onConfirm: () => { },
     onDismiss: () => { }
+}
+
+const defaultCourseModalWindowState: CourseModalWindowProps = {
+    mode: "add"
 }
 
 const ModalWindowWrapper = ({
     children
 }: { children: React.ReactNode }) => {
-    const [confirmActionModalWindowState, setConfirmActionModalWindowState] = useState<IConfirmActionModalWindowProps>(defaultConfirmActionModalWindowState)
+    const [confirmActionModalWindowState, setConfirmActionModalWindowState] = useState<ConfirmActionModalWindowProps>(defaultConfirmActionModalWindowState)
+    const [courseModalWindowState, setCourseModalWindowState] = useState<CourseModalWindowProps>(defaultCourseModalWindowState)
+
     const closeConfirmActionModalWindow = () => {
         setConfirmActionModalWindowState(defaultConfirmActionModalWindowState)
     }
+
+    const closeCourseModalWindow = () => {
+        setCourseModalWindowState(defaultCourseModalWindowState)
+    }
+
     return (
         <ModalWindowContext.Provider
             value={{
                 setConfirmActionModalWindowState,
-                closeConfirmActionModalWindow
+                closeConfirmActionModalWindow,
+                setCourseModalWindowState,
+                closeCourseModalWindow
             }}
         >
             <ConfirmActionModalWindow
                 {...{
                     onClose: closeConfirmActionModalWindow,
                     ...confirmActionModalWindowState
+                }}
+            />
+            <CourseModalWindow
+                {...{
+                    onClose: closeCourseModalWindow,
+                    ...courseModalWindowState
                 }}
             />
             {children}
