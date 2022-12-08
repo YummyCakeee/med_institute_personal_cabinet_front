51e2b121743beff3cede462a839390d9bafda9c4
Nikita <nikon15151@yandex.ru>
2022-12-08 22:22:15 +0300

    add: notification component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import 'styles/globals.css'
 import type { AppProps } from 'next/app'
 import ModalWindowWrapper from 'context/modalWindowContext'
+import { ReactNotifications } from 'react-notifications-component'
+import 'react-notifications-component/dist/theme.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ModalWindowWrapper>
-      <Component {...pageProps} />
-    </ModalWindowWrapper>
+    <>
+      <ReactNotifications />
+      <ModalWindowWrapper>
+        <Component {...pageProps} />
+      </ModalWindowWrapper>
+    </>
   )
 }
