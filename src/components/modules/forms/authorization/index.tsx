894fd218875cd08dfeeb73f3cbcdbcfd0d547ffe
Nikita <nikon15151@yandex.ru>
2022-12-08 22:09:19 +0300

    add: authorization form

---
+++ src/components/modules/forms/authorization/index.tsx
@@ -0,0 +1,69 @@
+import React from "react"
+import { Formik, Form, FormikValues, Field } from "formik"
+import Button from "components/elements/button/Button"
+import InputField from "components/elements/formikComponents/inputField/InputField"
+import utilStyles from "styles/utils.module.scss"
+import { composeValidators, maxLengthValueValidator, minLengthValueValidator, notEmptyValidator } from "utils/validators"
+
+type AutorizationFormProps = {
+    onSuccess: () => void
+}
+
+const AuthorizationForm = ({
+    onSuccess
+}: AutorizationFormProps) => {
+
+    const onSubmit = async (values: FormikValues) => {
+        onSuccess()
+    }
+
+    return (
+        <Formik
+            initialValues={{
+                login: "",
+                password: ""
+            }}
+            onSubmit={onSubmit}
+        >
+            {({ isSubmitting, isValid }) => (
+                <Form>
+                    <Field
+                        name="login"
+                        component={InputField}
+                        placeholder="Ваш логин"
+                        disabled={isSubmitting}
+                        validate={(value: string) =>
+                            composeValidators(value,
+                                notEmptyValidator,
+                                val => minLengthValueValidator(val, 3),
+                                val => maxLengthValueValidator(val, 20)
+                            )}
+                    />
+                    <Field
+                        name="password"
+                        component={InputField}
+                        placeholder="Пароль"
+                        type="password"
+                        disabled={isSubmitting}
+                        validate={(value: string) =>
+                            composeValidators(value,
+                                notEmptyValidator,
+                                val => minLengthValueValidator(val, 3),
+                                val => maxLengthValueValidator(val, 20)
+                            )}
+                    />
+                    <div className={utilStyles.form_button_container}>
+                        <Button
+                            title="Войти"
+                            size="small"
+                            type="submit"
+                            disabled={isSubmitting || !isValid}
+                        />
+                    </div>
+                </Form>
+            )}
+        </Formik>
+    )
+}
+
+export default AuthorizationForm
