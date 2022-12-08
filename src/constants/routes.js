9ec4a6eb78a707bac69d17109fccd7745d2257c9
Nikita <nikon15151@yandex.ru>
2022-12-08 22:11:54 +0300

    add: path routes constants

---
+++ src/constants/routes.js
@@ -0,0 +1,6 @@
+export const ROUTE_REGISTRATION = "/registration"
+export const ROUTE_COURSES = "/courses"
+export const ROUTE_EDUCATIONAL_PROGRAMS = "/educationalPrograms"
+export const ROUTE_USERS = "/users"
+export const ROUTE_TESTS_COLLECTION = "/testsCollection"
+export const ROUTE_PROFILE = "/"
