6465a091e43c51bbefcf5c1a7702fa2b4c8baf43
Nikita <nikon15151@yandex.ru>
2022-12-08 22:08:27 +0300

    add: requests middleware

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { ROUTE_REGISTRATION } from "constants/routes";
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+    return NextResponse.rewrite(new URL(ROUTE_REGISTRATION, request.url))
+
+}
+
+export const config = {
+    matcher: [
+        //'/courses', 
+        //'/users'
+    ]
+}
