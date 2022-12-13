9bd23a245df380ddca9dd3fab6c9b9ef491c4695
Nikita <nikon15151@yandex.ru>
2022-12-13 15:27:49 +0300

    upd: personal info section

---
+++ src/components/templates/profile/index.tsx
@@ -0,0 +1,39 @@
+import Layout from "components/layouts/Layout"
+import Head from "next/head"
+import React from "react"
+import styles from "./ProfileTemplate.module.scss"
+import utilStyles from "styles/utils.module.scss"
+import ProfileInfoForm from "components/modules/forms/profileInfo"
+import ItemList from "components/modules/itemList"
+
+const ProfileTemplate = () => {
+
+    return (
+        <Layout>
+            <Head>
+                <title>Мой профиль</title>
+            </Head>
+            <div>
+                <p className={utilStyles.section_title}>Сертификаты</p>
+                <div className={styles.certificates}>
+                    <ItemList
+                        headers={[
+                            {
+                                title: "Название",
+                                field: "name"
+                            }
+                        ]}
+                    />
+                </div>
+            </div>
+            <div>
+                <p className={utilStyles.section_title}>Изменение личных данных</p>
+                <div className={styles.personal_data_form}>
+                    <ProfileInfoForm />
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default ProfileTemplate
