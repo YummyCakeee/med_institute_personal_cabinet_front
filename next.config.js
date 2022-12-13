50e182c8b92ed4e6f0a5cb645bb21f23832b87a9
Nikita <nikon15151@yandex.ru>
2022-12-13 15:13:58 +0300

    upd: images  test config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,27 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      'get.pxhere.com'
+    ],
+    imageSizes: [
+      16,
+      32,
+      48,
+      64,
+      96,
+      128,
+      138,
+      180,
+      256,
+      276,
+      360,
+      384,
+      414,
+      540,
+    ],
+  }
 }
 
 module.exports = nextConfig
