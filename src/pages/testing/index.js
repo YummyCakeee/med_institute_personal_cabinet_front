ae778504955c034b416525fbbde9198f3062aa2d
Nikita <nikon15151@yandex.ru>
2022-12-13 15:33:01 +0300

    upd: component rename

---
+++ src/pages/testing/index.js
@@ -0,0 +1,11 @@
+import TestingTemplate from "components/templates/testing"
+import React from "react"
+
+const Testing = () => {
+
+    return (
+        <TestingTemplate />
+    )
+}
+
+export default Testing
