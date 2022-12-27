6f72e1cf84f1352fab4aedbfe440214e6a4f95c1
Nikita <nikon15151@yandex.ru>
2022-12-27 21:14:46 +0300

    upd: get user info on success

--- src/components/templates/registration/index.tsx
+++ src/components/templates/registration/index.tsx
@@ -3,12 +3,17 @@ import { ROUTE_PROFILE } from "constants/routes"
 import Head from "next/head"
 import { useRouter } from "next/router"
 import React from "react"
+import { useDispatch } from "react-redux"
+import { AppDispatch } from "store"
+import { getUserInfo } from "store/userSlice"
 
 const RegistrationTemplate = () => {
 
     const router = useRouter()
+    const dispatch = useDispatch<AppDispatch>()
 
     const onSuccess = () => {
+        dispatch(getUserInfo())
         router.replace(ROUTE_PROFILE)
     }
 
