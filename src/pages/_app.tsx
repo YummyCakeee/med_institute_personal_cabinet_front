ee64677eb68357469b438ecc6970cbec51ef8e22
Nikita <nikon15151@yandex.ru>
2022-12-27 21:07:40 +0300

    upd: initial current user info get

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,22 @@ import type { AppProps } from 'next/app'
 import ModalWindowWrapper from 'context/modalWindowContext'
 import { ReactNotifications } from 'react-notifications-component'
 import 'react-notifications-component/dist/theme.css'
+import { useEffect } from "react"
+import { Provider } from 'react-redux'
+import store from 'store'
+import { getUserInfo } from 'store/userSlice'
+
+store.dispatch(getUserInfo())
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ReactNotifications />
-      <ModalWindowWrapper>
-        <Component {...pageProps} />
-      </ModalWindowWrapper>
+      <Provider store={store}>
+        <ReactNotifications />
+        <ModalWindowWrapper>
+          <Component {...pageProps} />
+        </ModalWindowWrapper>
+      </Provider>
     </>
   )
 }
