cf4c4026243c70b6d77cb6374b297f475e75293f
Nikita <nikon15151@yandex.ru>
2022-12-28 21:41:51 +0300

    upd: date time props

--- src/components/elements/datetime/index.tsx
+++ src/components/elements/datetime/index.tsx
@@ -3,28 +3,46 @@ const LibDatetime = dynamic(() => import('react-datetime'), { ssr: false })
 import { Moment } from 'moment';
 import "react-datetime/css/react-datetime.css"
 import styles from "./Datetime.module.scss"
+import 'moment/locale/ru';
 
 type DatetimeProps = {
     value: string | Date | Moment | undefined,
-    onChange: (value: string | Moment) => void
+    onChange: (value: string | Moment) => void,
+    time?: boolean,
+    label?: string
 }
 
 const Datetime = ({
     value,
     onChange,
+    time,
+    label,
     ...props
 }: DatetimeProps) => {
 
-    return <LibDatetime
-        {...{
-            className: styles.container,
-            closeOnClickOutside: true,
-            locale: "ru",
-            value,
-            onChange,
-            ...props
-        }}
-    />
+    const renderInput = (props: any, openCalendar: Function, closeCalandar: Function) => {
+        return (
+            <input {...props} className={styles.dateTime} />
+        )
+    }
+
+    return <div className={styles.container}>
+        {label &&
+            <div className={styles.label}>{label}</div>
+        }
+        <LibDatetime
+            {...{
+                closeOnClickOutside: true,
+                locale: "ru",
+                dateFormat: "DD.MM.YYYY",
+                timeFormat: time ? "HH:mm" : "",
+                value,
+                onChange,
+                renderInput,
+                ...props
+            }}
+        />
+    </div>
 }
 
 export default Datetime
