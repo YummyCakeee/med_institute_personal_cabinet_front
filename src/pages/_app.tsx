bb686e2efd2330d6e5316d7db5f62a956ad8375d
Nikita <nikon15151@yandex.ru>
2022-12-28 13:06:29 +0300

    upd: redux wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,22 +3,30 @@ import type { AppProps } from 'next/app'
 import ModalWindowWrapper from 'context/modalWindowContext'
 import { ReactNotifications } from 'react-notifications-component'
 import 'react-notifications-component/dist/theme.css'
-import { useEffect } from "react"
+import { useEffect, FC } from "react"
 import { Provider } from 'react-redux'
-import store from 'store'
 import { getUserInfo } from 'store/userSlice'
+import { wrapper } from 'store'
+import { injectStore } from 'utils/axios/interceptor'
 
-store.dispatch(getUserInfo())
+const App: FC<AppProps> = ({ Component, ...rest }: AppProps) => {
 
-export default function App({ Component, pageProps }: AppProps) {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  useEffect(() => {
+    injectStore(store)
+    store.dispatch(getUserInfo())
+  }, [store])
+
   return (
     <>
       <Provider store={store}>
         <ReactNotifications />
         <ModalWindowWrapper>
-          <Component {...pageProps} />
+          <Component {...props.pageProps} />
         </ModalWindowWrapper>
       </Provider>
     </>
   )
 }
+
+export default App
