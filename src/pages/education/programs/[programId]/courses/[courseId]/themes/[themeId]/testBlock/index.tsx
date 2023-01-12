d7c88e859a3832424eaabe0ea11c044bd7b4235e
Nikita <nikon15151@yandex.ru>
2023-01-12 22:50:47 +0300

    upd: test block data fetching

--- src/pages/education/programs/[programId]/courses/[courseId]/themes/[themeId]/testBlock/index.tsx
+++ src/pages/education/programs/[programId]/courses/[courseId]/themes/[themeId]/testBlock/index.tsx
@@ -1,11 +1,69 @@
 import TestBlockTemplate from "components/templates/education/program/course/theme/testBlock"
-import React from "react"
+import { SolvedTestType } from "components/templates/education/types"
+import LoadingErrorTemplate from "components/templates/loadingError"
+import UnauthorizedTemplate from "components/templates/unauthorized"
+import { ENDPOINT_EDUCATION } from "constants/endpoints"
+import { useRouter } from "next/router"
+import React, { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import { userSelector } from "store/userSlice"
+import axiosApi from "utils/axios"
 
 const TestBlock = () => {
 
+    const user = useSelector(userSelector)
+    const router = useRouter()
+    const [test, setTest] = useState<SolvedTestType>()
+    const [success, setSuccess] = useState<boolean>(true)
+    const [error, setError] = useState<string>("")
+
+    useEffect(() => {
+        if (user.authorized) {
+            const { programId, courseId, themeId } = router.query
+            const requestUrl = `${ENDPOINT_EDUCATION}/Programs/${programId}/Courses/${courseId}/Themes/${themeId}/TestBlock`
+            if (router.query.attempt === "new") {
+                axiosApi.post(requestUrl)
+                    .then(res => {
+                        setSuccess(true)
+                        setTest(res.data)
+                    })
+                    .catch(err => {
+                        setSuccess(false)
+                        setError(err.code)
+                    })
+            }
+            else {
+                axiosApi.get(`${requestUrl}/Active`)
+                    .then(res => {
+                        setSuccess(true)
+                        setTest(res.data)
+                    })
+                    .catch(err => {
+                        setSuccess(false)
+                        setError(err.code)
+                    })
+            }
+        }
+    }, [user.authorized, router.query])
+
 
     return (
-        <TestBlockTemplate />
+        <>
+            {user.authorized ?
+                <>
+                    {success && test ?
+                        <TestBlockTemplate
+                            test={test}
+                        /> :
+                        <LoadingErrorTemplate
+                            error={error}
+                        />
+                    }
+                </> :
+                <UnauthorizedTemplate />
+
+            }
+        </>
     )
 }
 
