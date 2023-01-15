7ae08b534003d995428c09fa702e636d17495dc8
Nikita <nikon15151@yandex.ru>
2023-01-15 21:26:39 +0300

    add: exercise component

---
+++ src/components/templates/education/program/course/theme/exercise/index.tsx
@@ -0,0 +1,266 @@
+import { SolvedTestType, UserExerciseType } from "components/templates/education/types"
+import { useModalWindowContext } from "context/modalWindowContext"
+import { useRouter } from "next/router"
+import React, { useEffect, useRef, useState } from "react"
+import styles from "./Exercise.module.scss"
+import cn from "classnames"
+import FileLoader from "components/modules/fileLoader"
+import { convertSecondsToFullTime } from "utils/formatters"
+import { Field, Form, Formik, FormikHelpers, FormikValues } from "formik"
+import InputField from "components/elements/formikComponents/inputField/InputField"
+import Layout from "components/layouts/Layout"
+import Head from "next/head"
+import Button from "components/elements/button/Button"
+import TextAreaField from "components/elements/formikComponents/textAreaField/TextAreaField"
+import { notEmptyValidator } from "utils/validators"
+import addNotification from "utils/notifications"
+import axiosApi from "utils/axios"
+import { ENDPOINT_EDUCATION } from "constants/endpoints"
+
+type ExerciseTemplateProps = {
+    test: SolvedTestType
+}
+
+const ExerciseTemplate = ({
+    test
+}: ExerciseTemplateProps) => {
+
+    const [leftTime, setLeftTime] = useState<number>(0)
+    const timeoutRef = useRef<NodeJS.Timeout>()
+    const [initialTest, setInitialTest] = useState<SolvedTestType>()
+    const [files, setFiles] = useState<File[]>([])
+    const router = useRouter()
+    const [currentExerciceIndex, setCurrentExerciceIndex] = useState(-1)
+    const {
+        setConfirmActionModalWindowState
+    } = useModalWindowContext()
+
+
+    useEffect(() => {
+        setInitialTest(test)
+        const endTime = new Date(test.endTestTime).getTime()
+        const currentTime = new Date().getTime()
+        const leftTime = Math.floor((endTime - currentTime) / 1000)
+        setLeftTime(leftTime)
+    }, [test])
+
+    useEffect(() => {
+        if (leftTime > 0) {
+            timeoutRef.current = setTimeout(() => {
+                setLeftTime(leftTime - 1)
+            }, 1000)
+        }
+        return () => clearTimeout(timeoutRef.current)
+    }, [leftTime, setLeftTime, timeoutRef])
+
+    const onFileUpload = async (
+        values: FormikValues,
+        helpers: FormikHelpers<{
+            fileName: string;
+            fileDescription: string;
+        }>) => {
+        if (!initialTest) return
+        if (!files.length) {
+            addNotification({ type: "danger", title: "Ошибка", message: "Вы не прикрепили файл" })
+            return
+        }
+        let serverFileName = ""
+        const fd = new FormData()
+        fd.append("file", files[0])
+        await axiosApi.post(`${ENDPOINT_EDUCATION}/UploadFile`, fd)
+            .then(res => {
+                serverFileName = res.data.filename
+            })
+            .catch(err => {
+                addNotification({ type: "danger", title: "Ошибка", message: `Не удалось загрузить файл:\n${err.code}` })
+                return
+            })
+        const { programId, courseId, themeId } = router.query
+        const data: UserExerciseType = {
+            ...initialTest.userExercises[currentExerciceIndex],
+            file: {
+                fileName: values.fileName,
+                fileDescription: values.fileDescription,
+                fileLink: serverFileName
+            }
+        }
+        await axiosApi.put(`${ENDPOINT_EDUCATION}/Programs/${programId}/Courses/${courseId}/Themes/${themeId}/TestBlock/Exercise`, data)
+            .then(res => {
+                setInitialTest({
+                    ...initialTest,
+                    userExercises: initialTest.userExercises.map((el, key) => {
+                        if (key !== currentExerciceIndex)
+                            return el
+                        return data
+                    })
+                })
+                addNotification({ type: "success", title: "Успех", message: "Файл загружен" })
+                setFiles([])
+                helpers.resetForm()
+            })
+            .catch(err => {
+                addNotification({ type: "danger", title: "Ошибка", message: `Не удалось загрузить файл:\n${err.code}` })
+            })
+    }
+
+    const onCommentSend = async (
+        values: FormikValues,
+        helpers: FormikHelpers<{ comment: string }>) => {
+
+    }
+
+    const onGiveExerciseAnswerClick = (index: number) => {
+        setCurrentExerciceIndex(index)
+        setFiles([])
+    }
+
+    return (
+        <Layout>
+            <Head>
+                <title>Упражнения</title>
+            </Head>
+            <div className={styles.container}>
+                <div className={styles.time_left_container}>
+                    <div className={styles.text}>Оставшееся время:</div>
+                    <div className={cn(
+                        styles.time,
+                        { [styles.time_out]: leftTime === 0 }
+                    )}>
+                        {convertSecondsToFullTime(leftTime)}
+                    </div>
+                </div>
+                {initialTest?.userExercises.map((userExercise, userExerciseKey) => (
+                    <div
+                        className={styles.exercise_container}
+                        key={userExerciseKey}
+                    >
+                        <div className={styles.exercise_section}>
+                            <div className={styles.exercise_title}>
+                                {`Упражнение ${userExerciseKey + 1}`}
+                            </div>
+                            <div className={styles.exercise_text}>
+                                {userExercise.exerciseText}
+                            </div>
+                            {userExercise.file ?
+                                <div className={styles.exercise_file_info}>
+                                    <div>Вы прикрепили файл</div>
+                                    <div className={styles.exercise_file_name}>Название: <span>{userExercise.file.fileName}</span></div>
+                                    <div className={styles.exercise_file_description}>Описание: <span>{userExercise.file.fileDescription}</span></div>
+                                </div> :
+                                <div className={styles.exercise_file_absent}>
+                                    Вы ещё не прикрепляли файл
+                                </div>
+                            }
+                            {currentExerciceIndex === userExerciseKey ?
+                                <div className={styles.exercise_file_new}>
+                                    <div className={styles.exercise_file_new_title}>Прекрепить новый файл</div>
+                                    <Formik
+                                        initialValues={{
+                                            fileName: "",
+                                            fileDescription: ""
+                                        }}
+                                        onSubmit={onFileUpload}
+                                    >
+                                        {({ isSubmitting, isValid }) => (
+                                            <>
+                                                <FileLoader
+                                                    {...{
+                                                        files,
+                                                        setFiles
+                                                    }}
+                                                />
+                                                <Form>
+                                                    <Field
+                                                        name="fileName"
+                                                        component={InputField}
+                                                        placeholder="Название файла"
+                                                        validate={notEmptyValidator}
+                                                        disabled={isSubmitting}
+                                                    />
+                                                    <Field
+                                                        name="fileDescription"
+                                                        component={TextAreaField}
+                                                        placeholder="Описание файла"
+                                                        validate={notEmptyValidator}
+                                                        disabled={isSubmitting}
+                                                    />
+                                                    <Button
+                                                        title="Отправить"
+                                                        size="small"
+                                                        disabled={isSubmitting || !isValid}
+                                                        type="submit"
+                                                    />
+                                                </Form>
+                                            </>
+                                        )}
+                                    </Formik>
+                                </div> :
+                                <div>
+                                    <Button
+                                        title={userExercise.file ? "Редактировать ответ" : "Добавить ответ"}
+                                        stretchable={true}
+                                        onClick={() => onGiveExerciseAnswerClick(userExerciseKey)}
+                                    />
+                                </div>
+                            }
+                        </div>
+                        <div className={styles.comments_section}>
+                            <div className={styles.comments_title}>Коммментарии к упражнению</div>
+                            <div>Комментарии преподавателя</div>
+                            <div className={styles.comments}>
+                                {userExercise.teacherComments?.map((comment, commentKey) => (
+                                    <div
+                                        className={styles.comment}
+                                        key={commentKey}
+                                    >
+                                        {comment}
+                                    </div>
+                                ))}
+                            </div>
+                            <div>Комментарии учащегося</div>
+                            <div className={styles.comments}>
+                                {userExercise.userComments?.map((comment, commentKey) => (
+                                    <div
+                                        className={styles.comment}
+                                        key={commentKey}
+                                    >
+                                        {comment}
+                                    </div>
+                                ))}
+                            </div>
+                            <div>
+                                <div>Добавить комментарий</div>
+                                <Formik
+                                    initialValues={{
+                                        comment: ""
+                                    }}
+                                    onSubmit={onCommentSend}
+                                >
+                                    {({ isSubmitting, isValid }) => (
+                                        <Form>
+                                            <Field
+                                                name="comment"
+                                                component={TextAreaField}
+                                                validate={notEmptyValidator}
+                                                disabled={isSubmitting}
+                                                placeholder="Ваш комментарий"
+                                            />
+                                            <Button
+                                                title="Отправить"
+                                                size="small"
+                                                type="submit"
+                                                disabled={isSubmitting || !isValid}
+                                            />
+                                        </Form>
+                                    )}
+                                </Formik>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </Layout>
+    )
+}
+
+export default ExerciseTemplate
