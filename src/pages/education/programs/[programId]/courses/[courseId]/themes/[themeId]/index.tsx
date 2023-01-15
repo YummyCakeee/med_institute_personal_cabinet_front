12c6c8d4acfc66a6a4604277ad453dd2bd1606cb
Nikita <nikon15151@yandex.ru>
2023-01-15 21:27:53 +0300

    upd: data fetching

--- src/pages/education/programs/[programId]/courses/[courseId]/themes/[themeId]/index.tsx
+++ src/pages/education/programs/[programId]/courses/[courseId]/themes/[themeId]/index.tsx
@@ -1,16 +1,22 @@
 import axios from "axios"
+import { ThemeType } from "components/templates/courses/types"
 import ThemeTemplate from "components/templates/education/program/course/theme"
 import { SolvedTestType, UserThemeType } from "components/templates/education/types"
 import LoadingErrorTemplate from "components/templates/loadingError"
+import { TestBlockType } from "components/templates/testing/types"
 import UnauthorizedTemplate from "components/templates/unauthorized"
-import { ENDPOINT_EDUCATION } from "constants/endpoints"
+import { ENDPOINT_EDUCATION, ENDPOINT_THEMES } from "constants/endpoints"
 import { useRouter } from "next/router"
 import React, { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import { userSelector } from "store/userSlice"
 import axiosApi from "utils/axios"
 
-const Course = () => {
+export type TestBlockInfoType = {
+    isFileTestBlock: boolean
+}
+
+const Theme = () => {
 
     const router = useRouter()
     const user = useSelector(userSelector)
@@ -19,34 +25,42 @@ const Course = () => {
     const [userTheme, setUserTheme] = useState<UserThemeType>()
     const [solvedTests, setSolvedTests] = useState<SolvedTestType[]>()
     const [activeTest, setActiveTest] = useState<SolvedTestType>()
+    const [testBlock, setTestBlock] = useState<TestBlockType>()
+
     useEffect(() => {
         const fetchData = async () => {
             if (user.authorized && router.isReady) {
                 const { programId, courseId, themeId } = router.query
                 const requestUrl = `${ENDPOINT_EDUCATION}/Programs/${programId}/Courses/${courseId}/Themes/${themeId}`
-                let theme: UserThemeType | undefined
+                let userTheme: UserThemeType | undefined
                 await axiosApi.get(requestUrl, { params: { userId: user.id } })
                     .then(res => {
-                        theme = (res.data as UserThemeType)
-                        setUserTheme(theme)
+                        userTheme = (res.data as UserThemeType)
                         setSuccess(true)
+                        setUserTheme(userTheme)
                     })
                     .catch(err => {
                         setSuccess(false)
                         setError(err.code)
                         return
                     })
-                if (theme?.theme.testBlockId !== undefined) {
+                if (userTheme?.theme.testBlockId !== undefined) {
                     await axios.all([
                         axiosApi.get(`${requestUrl}/TestBlock`),
-                        axiosApi.get(`${requestUrl}/TestBlock/Active`)
+                        axiosApi.get(`${requestUrl}/TestBlock/Active`),
+                        axiosApi.get(`${ENDPOINT_THEMES}/${themeId}`)
                     ])
-                        .then(axios.spread(({ data: solvedTests }, { status: activeTestStatus, data: activeTest }) => {
+                        .then(axios.spread((
+                            { data: solvedTests },
+                            { status: activeTestStatus, data: activeTest },
+                            { data: theme }
+                        ) => {
                             setSuccess(true)
                             setSolvedTests(solvedTests)
                             if (activeTestStatus === 200) {
                                 setActiveTest(activeTest)
                             }
+                            setTestBlock((theme as ThemeType).testBlock)
                         }))
                         .catch(err => {
                             setSuccess(false)
@@ -68,9 +82,12 @@ const Course = () => {
                         <>
                             {userTheme &&
                                 <ThemeTemplate
-                                    userTheme={userTheme}
-                                    solvedTests={solvedTests}
-                                    activeTest={activeTest}
+                                    {...{
+                                        userTheme,
+                                        solvedTests,
+                                        activeTest,
+                                        testBlock
+                                    }}
                                 />
                             }
                         </>
@@ -87,4 +104,4 @@ const Course = () => {
     )
 }
 
-export default Course
+export default Theme
