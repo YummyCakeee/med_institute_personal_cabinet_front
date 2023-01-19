3ea5c6f90e1a2da82df244f922aeb071169bc726
Nikita <nikon15151@yandex.ru>
2023-01-19 12:47:31 +0300

    upd: program report

--- src/components/templates/educationalPrograms/index.tsx
+++ src/components/templates/educationalPrograms/index.tsx
@@ -4,6 +4,7 @@ import Head from "next/head"
 import ItemList from "components/modules/itemList"
 import useEducationalPrograms from "./useEducationalPrograms"
 import { ProgramType } from "./types"
+import utilStyles from "styles/utils.module.scss"
 
 type EducationalProgramsTemplateProps = {
     educationalPrograms: ProgramType[]
@@ -20,7 +21,8 @@ const EducationalProgramsTemplate = ({
         onEducationalProgramEditClick,
         onEducationalProgramDeleteClick,
         onEducationalProgramCoursesClick,
-        onEducationalProgramStudentsClick
+        onEducationalProgramStudentsClick,
+        onEducationalProgramReportClick
     } = useEducationalPrograms()
 
     useEffect(() => {
@@ -32,51 +34,60 @@ const EducationalProgramsTemplate = ({
             <Head>
                 <title>Образовательные программы</title>
             </Head>
-            <ItemList
-                headers={[
-                    {
-                        title: "Название",
-                        field: "title",
-                        colSize: "400px"
-                    },
-                    {
-                        title: "Описание",
-                        field: "description",
-                        colSize: "800px"
-                    }
-                ]}
-                items={programs}
-                itemControlButtons={() => [
-                    {
-                        title: "Редактировать",
-                        onClick: onEducationalProgramEditClick,
-                        size: "small",
-                        stretchable: true
-                    },
-                    {
-                        title: "Удалить",
-                        onClick: onEducationalProgramDeleteClick,
-                        size: "small",
-                    },
-                    {
-                        title: "Курсы",
-                        onClick: onEducationalProgramCoursesClick,
-                        size: "small",
-                    },
-                    {
-                        title: "Обучающиеся",
-                        onClick: onEducationalProgramStudentsClick,
-                        stretchable: true
-                    }
-                ]}
-                controlButtonsBottom={[
-                    {
-                        title: "Добавить",
-                        onClick: onEducationalProgramAddClick,
-                        size: "small"
-                    }
-                ]}
-            />
+            <div className={utilStyles.title}>Образовательные программы</div>
+            <div className={utilStyles.section}>
+                <div className={utilStyles.section_title}>Список образовательных программ</div>
+                <ItemList
+                    headers={[
+                        {
+                            title: "Название",
+                            field: "title",
+                            colSize: "400px"
+                        },
+                        {
+                            title: "Описание",
+                            field: "description",
+                            colSize: "800px"
+                        }
+                    ]}
+                    items={programs}
+                    itemControlButtons={() => [
+                        {
+                            title: "Редактировать",
+                            onClick: onEducationalProgramEditClick,
+                            size: "small",
+                            stretchable: true
+                        },
+                        {
+                            title: "Удалить",
+                            onClick: onEducationalProgramDeleteClick,
+                            size: "small",
+                        },
+                        {
+                            title: "Курсы",
+                            onClick: onEducationalProgramCoursesClick,
+                            size: "small",
+                        },
+                        {
+                            title: "Обучающиеся",
+                            onClick: onEducationalProgramStudentsClick,
+                            stretchable: true
+                        },
+                        {
+                            title: "Отчёт",
+                            onClick: onEducationalProgramReportClick,
+                            size: "small"
+                        }
+                    ]}
+                    controlButtonsBottom={[
+                        {
+                            title: "Добавить",
+                            onClick: onEducationalProgramAddClick,
+                            size: "small"
+                        }
+                    ]}
+                />
+            </div>
         </Layout>
     )
 }
