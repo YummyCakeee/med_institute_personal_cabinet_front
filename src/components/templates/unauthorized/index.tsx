f6b0ae0d12a0ab462d967818fed30bc31b8dced5
Nikita <nikon15151@yandex.ru>
2023-01-20 17:48:33 +0300

    upd: auth process

--- src/components/templates/unauthorized/index.tsx
+++ src/components/templates/unauthorized/index.tsx
@@ -3,27 +3,38 @@ import { ROUTE_REGISTRATION } from "constants/routes"
 import Head from "next/head"
 import { useRouter } from "next/router"
 import React from "react"
+import { useSelector } from "react-redux"
+import { userSelector } from "store/userSlice"
 import utilStyles from "styles/utils.module.scss"
 import styles from "./UnauthorizedTemplate.module.scss"
 
 const UnauthorizedTemplate = () => {
 
     const router = useRouter()
+    const user = useSelector(userSelector)
 
     return (
         <>
             <Head>
-                <title>Необходима авторизация</title>
+                <title>{user.infoLoadStatus === "pending" ? "Выполнение входа в аккаунт" : "Необходима авторизация"}</title>
             </Head>
             <div className={styles.container}>
-                <div className={utilStyles.text_large}>
-                    Вы не вошли в аккаунт
-                </div>
-                <Button
-                    title="Войти"
-                    onClick={() => router.replace(ROUTE_REGISTRATION)}
-                    size="small"
-                />
+                {user.infoLoadStatus === "pending" ?
+                    <div className={utilStyles.text_large}>
+                        Подождите, выполняется вход в аккаунт
+                    </div> :
+                    <>
+                        <div className={utilStyles.text_large}>
+                            Вы не вошли в аккаунт
+                        </div>
+                        <Button
+                            title="Войти"
+                            onClick={() => router.replace(ROUTE_REGISTRATION)}
+                            size="small"
+                        />
+                    </>
+
+                }
             </div>
         </>
     )
