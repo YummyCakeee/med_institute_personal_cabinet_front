2f97dbe6682e2b5772e4a201cdbff6e6e7631acc
Nikita <nikon15151@yandex.ru>
2023-01-23 12:20:01 +0300

    upd: use memo for userInfo

--- src/components/templates/users/user/UserTemplate.tsx
+++ src/components/templates/users/user/UserTemplate.tsx
@@ -1,5 +1,5 @@
 import Layout from "components/layouts/Layout"
-import React from "react"
+import React, { useMemo } from "react"
 import utilStyles from "styles/utils.module.scss"
 import styles from "./UserTemplate.module.scss"
 import cn from "classnames"
@@ -18,50 +18,44 @@ const UserTemplate = ({
     user
 }: UserTemplateProps) => {
 
-    const userInfo = [
-        {
-            name: "Фамилия",
-            value: user.secondName,
-        },
-        {
-            name: "Имя",
-            value: user.firstName,
-        },
-        {
-            name: "Отчество",
-            value: user.lastName,
-        },
-        {
-            name: "Логин",
-            value: user.user?.userName,
-        },
-        {
-            name: "Email",
-            value: user.user?.email,
-        },
-        {
-            name: "Роли",
-            value: user.user?.userRoles?.map(el => el.role.name).join(', '),
-        },
-        {
-            name: "Доступ",
-            value: user.user?.lockoutEnd && new Date(user.user.lockoutEnd) > new Date() ? "Заблокирован" : "Открыт"
-        },
-    ]
+    const userInfo = useMemo(() => {
+        return [
+            {
+                name: "Фамилия",
+                value: user.secondName,
+            },
+            {
+                name: "Имя",
+                value: user.firstName,
+            },
+            {
+                name: "Отчество",
+                value: user.lastName,
+            },
+            {
+                name: "Логин",
+                value: user.user?.userName,
+            },
+            {
+                name: "Email",
+                value: user.user?.email,
+            },
+            {
+                name: "Роли",
+                value: user.user?.userRoles?.map(el => el.role.name).join(', '),
+            },
+            {
+                name: "Доступ",
+                value: user.user?.lockoutEnd && new Date(user.user.lockoutEnd) > new Date() ? "Заблокирован" : "Открыт"
+            },
+        ]
+    }, [user])
 
     return (
         <Layout>
             <Head>
                 <title>{`Пользователь ${user.lastName} ${user.firstName}`}</title>
             </Head>
-            <Link href={ROUTE_USERS}>
-                <div className={styles.back_to_user_list}>
-                    <ArrowIcon
-                        className={styles.back_to_user_list_arrow}
-                    />
-                    <p className={styles.back_to_user_list_text}>Список пользователей</p>
-                </div>
-            </Link>
             <div className={styles.personal_info_section}>
                 <div
                     className={utilStyles.section_title}>
