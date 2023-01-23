cc94889f6a413b3c221c8ae8d857559337cb94b2
Nikita <nikon15151@yandex.ru>
2023-01-23 12:18:13 +0300

    add: bread crumbs

---
+++ src/components/modules/breadCrumbs/index.tsx
@@ -0,0 +1,88 @@
+import { ArrowIcon } from "components/elements/icons"
+import { ROUTE_USERS } from "constants/routes"
+import Link from "next/link"
+import React, { useEffect, useMemo, useRef, useState } from "react"
+import { useSelector } from "react-redux"
+import { breadCrumbsSelector } from "store/breadCrumbsSlice"
+import styles from "./BreadCrumbs.module.scss"
+import cn from "classnames"
+
+const BreadCrumbs = () => {
+
+    const breadCrumbs = useSelector(breadCrumbsSelector).breadCrumbs
+    const [activeBreadCrumbIndex, setActiveBreadCrumbIndex] = useState<number>()
+    const ref = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+
+        const handleLeave = (e: MouseEvent) => {
+            if (ref.current && !ref.current.contains(e.target as Node)) {
+                setActiveBreadCrumbIndex(undefined)
+            }
+        }
+        document.addEventListener("mouseover", handleLeave)
+
+        return () => {
+            document.removeEventListener("mouseover", handleLeave)
+        }
+    }, [ref])
+
+    const lastBreadCrumb = useMemo(() => {
+        return breadCrumbs.length ? breadCrumbs[breadCrumbs.length - 1] : undefined
+    }, [breadCrumbs])
+
+    return (
+        <div
+            className={styles.bread_crumbs_container}
+            ref={ref}
+        >
+            {breadCrumbs.slice(0, breadCrumbs.length - 1).map((el, key) => (
+                <Link
+                    href={el.route}
+                    key={key}>
+                    <div
+                        className={cn(
+                            styles.bread_crumb,
+                            { [styles.bread_crumb_active]: activeBreadCrumbIndex === key }
+                        )}
+                        onMouseEnter={() => setActiveBreadCrumbIndex(key)}
+                    >
+                        <ArrowIcon
+                            className={styles.bread_crumb_arrow}
+                        />
+                        <p
+                            className={styles.bread_crumb_text}
+                            title={el.title}
+                        >
+                            {el.title}
+                        </p>
+                    </div>
+                </Link>
+            ))}
+            {lastBreadCrumb &&
+                <Link href={lastBreadCrumb.route}>
+                    <div
+                        className={cn(
+                            styles.bread_crumb,
+                            { [styles.bread_crumb_active]: activeBreadCrumbIndex === breadCrumbs.length - 1 },
+                            { [styles.bread_crumb_last_active]: activeBreadCrumbIndex === undefined }
+                        )}
+                        onMouseEnter={() => setActiveBreadCrumbIndex(breadCrumbs.length - 1)}
+                    >
+                        <ArrowIcon
+                            className={styles.bread_crumb_arrow}
+                        />
+                        <p
+                            className={styles.bread_crumb_text}
+                            title={lastBreadCrumb.title}
+                        >
+                            {lastBreadCrumb.title}
+                        </p>
+                    </div>
+                </Link>
+            }
+        </div>
+    )
+}
+
+export default BreadCrumbs
